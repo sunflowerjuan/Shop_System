9a386946485cdb1de200090f86565a516d2a7412
Katalina Torres <lakatofo2004@gmail.com>
2024-04-14 17:25:38 -0500

    Cambio header a gbc

--- src/co/edu/uptc/view/HeaderPanel.java
+++ src/co/edu/uptc/view/HeaderPanel.java
@@ -8,20 +8,27 @@ public class HeaderPanel extends JPanel {
     private JButton exitButton;
 
     public HeaderPanel() {
-        setLayout(new GridLayout(1, 2));
+        setLayout(new GridBagLayout());
         setBackground(new Color(0, 151, 178));
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 1.0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
 
         imageShop = new JLabel();
         ImageIcon icon = new ImageIcon("img/header.png");
         imageShop.setIcon(icon);
-        add(imageShop);
+        add(imageShop, gbc);
 
-        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        buttonPanel.setBackground(new Color(0, 151, 178));
+        gbc.gridx = 1;
+        gbc.weightx = 0.0;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new Insets(0, 0, 0, 20);
+
         exitButton = new JButton("Salir");
         exitButton.setPreferredSize(new Dimension(60, 30));
-        buttonPanel.add(exitButton);
-
-        add(buttonPanel);
+        add(exitButton, gbc);
     }
 }
